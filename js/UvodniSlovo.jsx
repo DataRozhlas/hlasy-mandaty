f948f8976e1c413c8199b452cba20d375575d965
Petr Kočí <petr.koci@samizdat.cz>
2021-03-22 06:55:11 +0100

    procenta u stran

--- js/UvodniSlovo.jsx
+++ js/UvodniSlovo.jsx
@@ -3,7 +3,12 @@ import GrafStran from "./GrafStran.jsx";
 import SimpleSelect from "./SimpleSelect.jsx";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
+const postupuji = vysledky.CR.strana.filter(strana => {
+    
+})
 
 function UvodniSlovo({ krok, rok, setRok, vysledky }) {
   switch (krok) {
@@ -18,10 +23,7 @@ function UvodniSlovo({ krok, rok, setRok, vysledky }) {
           </Typography>
           <Typography>Které volby si s námi chcete přepočítat?</Typography>
           <Box mb={2}>
-            <SimpleSelect
-              rok={rok}
-              setRok={setRok}
-            ></SimpleSelect>
+            <SimpleSelect rok={rok} setRok={setRok}></SimpleSelect>
           </Box>
           <GrafStran
             vysledky={vysledky}
@@ -37,7 +39,46 @@ function UvodniSlovo({ krok, rok, setRok, vysledky }) {
         </>
       );
     case 2:
-      return <div>povidy2</div>;
+      return (
+        <>
+          {" "}
+          <Typography paragraph={true}>
+            {`V roce ${rok}, který jste si vybrali, překonalo hranici pro vstup do sněmovny ${postupuji.length} stran:`}
+          </Typography>
+          <Box display="flex" flexWrap="wrap" justifyContent="center" mb={2}>
+            {postupuji.map((strana, i) => {
+              return (
+                <Card
+                  key={strana._attributes.KSTRANA}
+                  variant="outlined"
+                  style={{ margin: "0.2rem" }}
+                >
+                  <CardContent>
+                    <Typography
+                      variant="subtitle2"
+                      align="center"
+                      gutterBottom={true}
+                    >
+                      {i + 1}. {strana._attributes.NAZ_STR}
+                    </Typography>
+                    <Typography variant="body2" align="center">
+                      {`${strana.HODNOTY_STRANA._attributes.PROC_HLASU.toLocaleString(
+                        "cs-CZ"
+                      )} %`}
+                    </Typography>
+
+                    <Typography variant="body2" align="center">
+                      {`${strana.HODNOTY_STRANA._attributes.HLASY.toLocaleString(
+                        "cs-CZ"
+                      )} hlasů`}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </Box>
+        </>
+      );
     case 3:
       return <div>povidy3</div>;
     case 4:
